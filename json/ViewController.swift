aa4a7e825c9ec87994162804f0dae521d19a8801
Mark Woodward <markw@digitalbridge.com.au>
2016-07-04 13:22:26 +1000

    Initial Commit

---
+++ json/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  json
+//
+//  Created by Mark Woodward on 4/07/2016.
+//  Copyright © 2016 Mark Woodward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
